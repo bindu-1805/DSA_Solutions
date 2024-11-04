f4317c246c873ff2445f47560a86be14d1a4e926
Bindu Madhavi V <bindu2003madhavi@gmail.com>
2024-11-04 19:37:52 +0530

    Add easy level probs

---
+++ Easy/singleNo.cpp
@@ -0,0 +1,11 @@
+//LC 136
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+        for(int val : nums){
+            ans = ans ^ val;
+        }
+        return ans;
+    }
+};
