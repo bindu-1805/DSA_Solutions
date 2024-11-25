de03468ae932e087be6c30d1b68606fa9874c7c7
Bindu Madhavi V <137310333+bindu-1805@users.noreply.github.com>
2024-11-25 20:15:19 +0530

    Create LC9.cpp

---
+++ Easy/LC9.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+
+    int reverse(int n) {
+        int rev=0;
+        while(n!=0){
+            int rem=n%10;
+            if(rev>INT_MAX/10 || rev<INT_MIN/10){
+                return 0;
+            }
+            rev= rev * 10 + rem;
+            n=n/10;
+        }
+        return rev;
+    }
+
+    bool isPalindrome(int n) {
+        if(n<0) return false;
+        int revNum=reverse(n);
+        return n==revNum;
+    }
+};
